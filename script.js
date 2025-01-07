172231f2a7b79ee8853ad3e275c09e648ea32c63
Angshul Sana <167230304+angshul004@users.noreply.github.com>
2025-01-07 18:18:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ document.getElementById('toggleButton').addEventListener('click', function() {
     var modules = document.getElementById('modules');
     modules.classList.toggle('show');
 });
+
+function openPDF() {
+    window.open('pdfviewer.html', '_blank');
+}
