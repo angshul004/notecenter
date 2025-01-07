5c814b2ee2d35808210c03f5823d4bcfb3eb8fe0
Angshul Sana <167230304+angshul004@users.noreply.github.com>
2025-01-07 19:08:21 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('toggleButton').addEventListener('click', function() {
-    var modules = document.getElementById('modules');
-    modules.classList.toggle('show');
-});
-
-function openPDF() {
-    window.open('https://docs.google.com/gview?url=https://github.com/angshul004/notecenter/raw/main/sdg.pdf&embedded=true', '_blank');
-}
